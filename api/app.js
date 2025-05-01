d5eef4b48bbdd72186fe91741000c082441e2e42
Kirill Snopko <kirillsnopko@gmail.com>
2025-05-01 17:11:20 +0300

    init

---
+++ api/app.js
@@ -0,0 +1,28 @@
+import gplay from "google-play-scraper";
+
+// Define the endpoint
+module.exports = async = async (req, res) => {
+    const { appid, country } = req.query;
+
+    // Validate required parameter
+    if (!appid) {
+        return res.status(400).json({ error: 'Missing required parameter: appid' });
+    }
+
+    try {
+        // Fetch app details from Google Play Store
+        var reviews = await gplay.reviews({
+            appId: appid,
+            country: country,
+            sort: gplay.sort.NEWEST,
+            num: 1000
+        });
+        
+        console.log(reviews);
+        res.json(reviews);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch app details' });
+    }
+};
+
---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+//const gplay = require('google-play-scraper');
+const app = express();
+const PORT = 3000;
+import gplay from "google-play-scraper";
+
+// Define the endpoint
+app.get('/app', async (req, res) => {
+    const { appid, country } = req.query;
+
+    // Validate required parameter
+    if (!appid) {
+        return res.status(400).json({ error: 'Missing required parameter: appid' });
+    }
+
+    try {
+        // Fetch app details from Google Play Store
+        var reviews = await gplay.reviews({
+            appId: appid,
+            country: country,
+            sort: gplay.sort.NEWEST,
+            num: 1000
+        });
+        
+        console.log(reviews);
+        res.json(reviews);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch app details' });
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
