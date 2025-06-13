878029310e2bf3052b08240f04fdbe3508160bf3
Kirill Snopko <kirillsnopko@gmail.com>
2025-06-13 20:32:24 +0300

    add lang

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ import gplay from "google-play-scraper";
 
 // Define the endpoint
 app.get('/app', async (req, res) => {
-    const { appid, country } = req.query;
+    const { appid, country, lang } = req.query;
 
     // Validate required parameter
     if (!appid) {
         return res.status(400).json({ error: 'Missing required parameter: appid' });
     }
 
+    const options = {
+        num: 1000,
+        sort: gplay.sort.NEWEST,
+        appId: appid,
+    };
+
+    if (country) options.country = country;
+    if (lang) options.lang = lang;
+
     try {
         // Fetch app details from Google Play Store
-        var reviews = await gplay.reviews({
-            appId: appid,
-            country: country,
-            sort: gplay.sort.NEWEST,
-            num: 1000
-        });
-        
+        var reviews = await gplay.reviews(options);
+
         console.log(reviews);
         res.json(reviews);
     } catch (err) {
